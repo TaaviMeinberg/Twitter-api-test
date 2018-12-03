b6ec50a70d3a1d9b35d151140ab34db80bfaa12f
mhaava <mhaava@tlu.ee>
2018-12-03 12:30:16 +0200

    listud failid

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  include "funtions.php";
+?>
+<html>
+  <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Page Title</title>
+  </head>
+  <body>
+    <div id="content">...</div>
+  </body>
+</html>
