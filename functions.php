f8dccc8b86f7ba6edefd431e583531801c20a2a5
mhaava <mhaava@tlu.ee>
2018-12-10 14:35:37 +0200

    load more funktsionaalsus

--- functions.php
+++ functions.php
@@ -7,12 +7,31 @@
 
   require 'autoload.php';
   use Abraham\TwitterOAuth\TwitterOAuth;
-
+  
   $connection = new TwitterOAuth($consumerKey, $consumerKeySecret, $accessToken, $accessTokenSecret);
   $content = $connection->get("account/verify_credentials");
 
   $statuses = $connection->get("statuses/home_timeline", ["count" => 1, "exclude_replies" => true]);
-  $search = $connection->get("search/tweets", ["q" => "%23Estonia", "result_type"=>"recent", "count" => 1, "include_entities"=> "false"]);
+if (isset($_REQUEST["q"])) {
+  $search = $connection->get("search/tweets", ["q" => "%23Estonia", "result_type"=>"recent", "count" => 6, "max_id" => $_REQUEST["q"]]);
+}
 
-  print_r($search);
+
+  #THIS FOREACH WAS MADE POSSIBLE THANKS TO MAREK KIVIKINK
+  $i=1;
+  foreach ($search->statuses as $item) {
+      $id_str = $item->id_str;
+      if ($i<6) {
+        $response = file_get_contents("https://publish.twitter.com/oembed?url=https://twitter.com/Interior/status/$id_str");
+        $jsonObj =json_decode($response);
+        echo('<div id="'. $id_str .'">' . $jsonObj->html. '</div>');
+      } else {
+        echo('<div id="'. $id_str .'"></div>');
+      }
+    $i++;
+  }
+
+
+
+
 ?>
