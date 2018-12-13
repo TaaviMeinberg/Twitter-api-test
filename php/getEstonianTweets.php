64b0865b6758b067a0f3c3c6f42835f0c010ee6d
mhaava <mhaava@tlu.ee>
2018-12-13 19:31:17 +0200

    Eesti tweetide juurde laadimine

--- php/getEstonianTweets.php
+++ php/getEstonianTweets.php
@@ -1,23 +1,24 @@
 <?php
-  require './php/auth.php';
+  require './auth.php';
   $content = $connection->get("account/verify_credentials");
 
   $statuses = $connection->get("statuses/home_timeline", ["count" => 1, "exclude_replies" => true]);
-  $search = $connection->get("search/tweets", ["q" => 'geocode:58.595272,25.01360,200km', "result_type"=>"recent","lang"=>"et", "count" => 5]);
-
-  #THIS FOREACH WAS MADE POSSIBLE THANKS TO MAREK KIVIKINK
-  foreach ($search->statuses as $item) {
-    if(isset($item->id_str)){
-      $id_str = $item->id_str;
-      echo $id_str;
-      $response = file_get_contents("https://publish.twitter.com/oembed?url=https://twitter.com/Interior/status/$id_str");
-      $jsonObj =json_decode($response);
-      echo($jsonObj->html);
-      echo('<br>');
-    }
+  if (isset($_REQUEST["q"])) {
+    $search = $connection->get("search/tweets", ["q" => 'geocode:58.595272,25.01360,200km', "result_type"=>"recent","lang"=>"et", "count" => 5, "max_id" => $_REQUEST["q"]]);
   }
-
-
-
-
+  
+  
+    #THIS FOREACH WAS MADE POSSIBLE THANKS TO MAREK KIVIKINK
+    $i=1;
+    foreach ($search->statuses as $item) {
+        $id_str = $item->id_str;
+        if ($i<6) {
+          $response = file_get_contents("https://publish.twitter.com/oembed?url=https://twitter.com/Interior/status/$id_str");
+          $jsonObj =json_decode($response);
+          echo('<div id="'. $id_str .'">' . $jsonObj->html. '</div>');
+        } else {
+          echo('<div id="'. $id_str .'"></div>');
+        }
+      $i++;
+    }
 ?>
